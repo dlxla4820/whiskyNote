21c6790c1d3833011a5d9c2b25eb0397196c97c4
dlxla4820 <70089910+dlxla4820@users.noreply.github.com>
2024-11-12 09:55:41 +0900

    Add files via upload

---
+++ src/main/java/develop/wiskyNote/WiskyNoteApplication.java
@@ -0,0 +1,13 @@
+package develop.wiskyNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiskyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiskyNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/develop/wiskyNote/WiskyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.wiskyNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiskyNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
