94474518c2339d5b0386e9c9dd3cd7f531293b4b
dlxla4820 <gorhfqhk@gmail.com>
2024-11-23 20:04:42 +0900

    enums 파일 구조 변경, 유저 서비스 수정
    - deviceID 저장 로직 추가

--- src/main/java/develop/whiskyNote/service/UserService.java
+++ src/main/java/develop/whiskyNote/service/UserService.java
@@ -15,6 +15,10 @@ public class UserService {
 
     public ResponseDto<?> saveUser(UserRequestDto requestBody){
         if(requestBody.getDeviceId() == null)
-            
+            return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(),Description.FAIL,ErrorCode.PARAMETER_INVALID_SPECIFIC.getErrorCode(), String.format(ErrorCode.PARAMETER_INVALID_SPECIFIC.getErrorDescription(), "device_id"));
+        UUID userUuid = userInfoRepository.getUuidByDeviceId(requestBody.getDeviceId());
+        if(userUuid == null)
+            userInfoRepository.saveUser(requestBody);
+        return new ResponseDto<>(HttpStatus.OK.value(), Description.SUCCESS);
     }
 }
