859ed5ebd85a5911c0cdf184879bd81f717550b1
dlxla4820 <gorhfqhk@gmail.com>
2024-12-08 23:47:12 +0900

    fix : 리뷰 요청데이터 수정

--- src/main/java/develop/whiskyNote/dto/ReviewCreateRequestDto.java
+++ src/main/java/develop/whiskyNote/dto/ReviewCreateRequestDto.java
@@ -8,23 +8,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.StringJoiner;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ReviewCreateRequestDto {
-    private String title;
     private String content;
-    MultipartFile[] images;
-
+    private boolean isAnonymous;
+    private LocalDate openDate;
+    private MultipartFile[] images;
+    private String tags;
+    private Long score;
     @Override
     public String toString() {
         return new StringJoiner(", ", ReviewCreateRequestDto.class.getSimpleName() + "[", "]")
-                .add("title='" + title + "'")
                 .add("content='" + content + "'")
+                .add("isAnonymous='" + isAnonymous + "'")
                 .add("images.count() =" + Arrays.stream(images).count())
+                .add("openDate='" + openDate + "'")
+                .add("tags='" + tags + "'")
+                .add("score='" + score + "'")
                 .toString();
     }
 }
