f8c4f27f8b3e8456a36204f4745789910336bdd6
dlxla4820 <gorhfqhk@gmail.com>
2024-12-12 17:40:51 +0900

    fix : insert, update dto 추가

---
+++ src/main/java/develop/whiskyNote/dto/ReviewUpsertRequestDto.java
@@ -0,0 +1,37 @@
+package develop.whiskyNote.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ReviewUpsertRequestDto {
+    private String content;
+    private Boolean isAnonymous;
+    private LocalDate openDate;
+    private MultipartFile[] images;
+    private List<String> tags;
+    private Long score;
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ReviewUpsertRequestDto.class.getSimpleName() + "[", "]")
+                .add("content='" + content + "'")
+                .add("isAnonymous='" + isAnonymous + "'")
+                .add("images.count() =" + Arrays.stream(images).count())
+                .add("openDate='" + openDate + "'")
+                .add("tags='" + tags + "'")
+                .add("score='" + score + "'")
+                .toString();
+    }
+}
